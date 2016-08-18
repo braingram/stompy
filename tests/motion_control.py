6435cad8163715c650985701410befa06b0bae83
brett <brettgraham@gmail.com>
2016-08-17 22:07:34 -0400

    working on motion control planning

--- tests/motion_control.py
+++ tests/motion_control.py
@@ -10,12 +10,12 @@ S-curves have 7 phases
 - decrease deceleration
 
 case 1
- - accel then decel
- - accel cruise then decel
+ - accel
 case 2
  - accel cruise
 case 3
- - accel
+ - accel then decel
+ - accel cruise then decel
 
 don't enforce max accel
 """
@@ -24,61 +24,103 @@ import numpy
 import pylab
 
 
-s = 0.0  # meters, starting position
-e = 0.5  # meters, ending position
-t = 1.0  # seconds, time to complete movement
-dt = 0.1  # seconds
-n = int(numpy.ceil(t / float(dt)))
-
-a = 10.0  # m / s ** 2, acceleration
-d = 10.0  # m / s ** 2, deceleration
-#j = 100.0  # m / s ** 3, jerk
-
-v_s = 0.  # m / s, starting velocity
-v_e = 1.  # m / s, ending velocity
-
-v_m = 10.0  # m / s, maximum
-
-d = abs(e - s)  # m, distance traveled
-v_avg = d / t  # m / s, average velocity
-
 approximate = lambda v, t, e=0.001: abs(v - t) < e
 
-# case 3: accel
-acc = (v_e - v_s) / t
-ep = 0.5 * acc * t * t + v_s * t + s
-print(acc, ep)
-if approximate(ep, e):
-    ts = numpy.linspace(0, t, n)
-    vs = numpy.linspace(v_s, v_e, n)
+
+def plot(ts, vs, xs, case, e, t, v_e):
+    pylab.subplot(211)
+    pylab.plot(ts, xs, color='b')
+    pylab.scatter(ts, xs, color='b')
+    pylab.axhline(e, color='b')
+    pylab.axvline(t, color='k')
+    xl = pylab.xlim()
+    pylab.xlim(xl[0], xl[1]+0.5)
+    yl = pylab.ylim()
+    pylab.ylim(yl[0], yl[1]+0.5)
+    pylab.title("Case %s: positon vs time" % case)
+    pylab.subplot(212)
+    pylab.plot(ts, vs, color='g')
+    pylab.scatter(ts, vs, color='g')
+    pylab.axhline(v_e, color='g')
+    pylab.axvline(t, color='k')
+    xl = pylab.xlim()
+    pylab.xlim(xl[0], xl[1]+0.5)
+    yl = pylab.ylim()
+    pylab.ylim(yl[0], yl[1]+0.5)
+    pylab.title("Case %s: velocity vs time" % case)
+    pylab.show()
+
+
+def test(
+        s=0.0, e=3.0, t=2.0, dt=0.1, v_s=0., v_e=1.,
+        a_max=10.0, d_max=10.0, v_max=10.0):
+    ts, vs, case = compute_motion_profile(
+        s, e, t, dt, v_s, v_e, a_max, d_max, v_max)
     xs = numpy.cumsum(vs * dt)
+    plot(ts, vs, xs, case, e, t, v_e)
 
-# from: firmware/tinyg/plan_line.c[161]
-#t_a = 2 * numpy.sqrt(abs(v_s - v_c) / jerk)
-#t_d = 2 * numpy.sqrt(abs(v_c - v_e) / jerk)
+def compute_motion_profile(
+        s=0.0, e=3.0, t=2.0, dt=0.1,
+        v_s=0., v_e=1.,
+        a_max=10.0, d_max=10.0, v_max=10.0):
+    """
+    Compute a motion profile
 
-#t_a = abs(v_c - v_s) / a  # s, acceleration time
-#t_d = abs(v_c - v_e) / d  # s, deceleration time
+    Args:
+    ------
+        s : starting position
+        e : ending position
+        t : time to complete movement
+        dt : time resolution of movement
+        v_s : starting velocity
+        v_e : ending velocity
+        a_max : maximum acceleration
+        d_max : maximum deceleration
+        v_max : maximum velocity
+    """
+    n = int(numpy.ceil(t / float(dt)))
+    d = abs(e - s)
+    v_avg = d / t
+
+    ts = numpy.linspace(0, t, n)
+    case = None
+    acc = abs(v_e - v_s) / t  # TODO cap acceleration
+    if acc > a_max:
+        raise Exception(
+            "Movement requires acceleration[%s] over max [%s]"
+            % (acc, a_max))
+
+    # case 1: accel
+    print("case 1?")
+    ep = 0.5 * acc * t * t + v_s * t + s
+    print("ep, e: %s, %s" % (ep, e))
+    if approximate(ep, e):
+        case = 1
+        vs = numpy.linspace(v_s, v_e, n)
+        return ts, vs, case
+
+    if v_s == v_e and s != e:
+        # accel then decel
+        raise NotImplementedError
+    # case 2: accel cruise
+    print("case 2?")
+    acc_t = (e - s - v_e * t) / (0.5 * abs(v_e - v_s) + v_s - v_e)
+    print("acc_t: %s" % acc_t)
+    if acc_t > 0.:
+        acc = abs(v_e - v_s) / acc_t
+        c_t = t - acc_t
+        if c_t > 0:
+            ep = c_t * v_e + 0.5 * acc * acc_t * acc_t + v_s * acc_t + s
+            if c_t > 0 and approximate(ep, e):
+                case = 2
+                vs = numpy.ones_like(ts) * v_e
+                am = ts < acc_t
+                vs[am] = numpy.linspace(v_s, v_e, sum(am))
+                return ts, vs, case
+
+    # case 3: accel then decel, accel cruise then decel
+    raise NotImplementedError
 
 
-pylab.subplot(211)
-pylab.plot(ts, xs, color='b')
-pylab.scatter(ts, xs, color='b')
-pylab.axhline(e, color='b')
-pylab.axvline(t, color='k')
-xl = pylab.xlim()
-pylab.xlim(xl[0], xl[1]+0.5)
-yl = pylab.ylim()
-pylab.ylim(yl[0], yl[1]+0.5)
-pylab.title("positon vs time")
-pylab.subplot(212)
-pylab.plot(ts, vs, color='g')
-pylab.scatter(ts, vs, color='g')
-pylab.axhline(v_e, color='g')
-pylab.axvline(t, color='k')
-xl = pylab.xlim()
-pylab.xlim(xl[0], xl[1]+0.5)
-yl = pylab.ylim()
-pylab.ylim(yl[0], yl[1]+0.5)
-pylab.title("velocity vs time")
-pylab.show()
+#test(s=0.0, e=0.5, t=1.0, v_e=1.0)  # case 3
+test(s=0.0, e=2.0, t=2.0, v_e=1.0)  # case 2
