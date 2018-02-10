fcf49b79373c17fbd7b3a15a8b573c904a1b4549
Brett Graham <brettgraham@gmail.com>
2018-02-10 09:25:39 -0500

    adding log module

--- stompy/__init__.py
+++ stompy/__init__.py
@@ -3,5 +3,6 @@
 from . import controllers
 from . import joystick
 from . import leg
+from . import log
 
-__all__ = ['controllers', 'joystick', 'leg']
+__all__ = ['controllers', 'joystick', 'leg', 'lg']
---
+++ stompy/log.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+"""
+Write timestamped dicts to pickle file
+"""
+
+import atexit
+import logging
+import os
+import cPickle as pickle
+import time
+
+
+class Logger(object):
+    def __init__(self, directory, events_per_file=10000):
+        self.level = logging.WARNING
+        self._dir = directory
+        self._events = []
+        self._file_index = 0
+        self.events_per_file = events_per_file
+
+    def _write_events(self):
+        if len(self._events) == 0:
+            return
+        # write to disk
+        if not os.path.exists(self._dir):
+            os.makedirs(self._dir)
+        # directory/index_timestamp.p
+        fn = '%04i_%s.p' % (self._file_index, int(time.time()))
+        fp = os.path.join(self._dir, fn)
+        with open(fp, 'wb') as f:
+            pickle.dump(self._events, f)
+        self._events = []
+        self._file_index += 1
+
+    @property
+    def directory(self):
+        return self._dir
+
+    @directory.setter
+    def directory(self, directory):
+        self._write_events()
+        self._dir = directory
+
+    def log(self, event, level):
+        if level < self.level:
+            return
+        if not isinstance(event, dict):
+            event = {'event': event}
+        if 'timestamp' not in event:
+            event['timestamp'] = time.time()
+        self._events.append(event)
+        if len(self._events) >= self.events_per_file:
+            self._write_events()
+
+    def critical(self, event):
+        self.log(event, logging.CRITICAL)
+
+    def error(self, event):
+        self.log(event, logging.ERROR)
+
+    def warning(self, event):
+        self.log(event, logging.WARNING)
+
+    def info(self, event):
+        self.log(event, logging.INFO)
+
+    def debug(self, event):
+        self.log(event, logging.DEBUG)
+
+
+logger = Logger(os.path.join('stompy_logs', str(int(time.time()))))
+
+critical = logger.critical
+error = logger.error
+warning = logger.warning
+info = logger.info
+debug = logger.debug
+
+atexit.register(logger._write_events)
