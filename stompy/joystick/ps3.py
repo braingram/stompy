441e431558d7713e25a95e4277b5d551cd783b76
Brett Graham <brettgraham@gmail.com>
2018-10-15 07:34:19 -0400

    starting dynamic remap of ps3 code-to-button map

--- stompy/joystick/ps3.py
+++ stompy/joystick/ps3.py
@@ -32,50 +32,53 @@ EVS = dict([
     (0x1f, "Max"),
     (0x1f+1, "Current")])
 
-KEYS = dict([  # 0x01, 17
-    (0x120, 'select'),
-    (0x121, 'left_thumb'),
-    (0x122, 'right_thumb'),
-    (0x123, 'start'),
-    (0x124, 'up'),
-    (0x125, 'right'),
-    (0x126, 'down'),
-    (0x127, 'left'),
-    (0x128, 'two_left'),
-    (0x129, 'two_right'),
-    (0x12a, 'one_left'),
-    (0x12b, 'one_right'),
-    (0x12c, 'triangle'),
-    (0x12d, 'circle'),
-    (0x12e, 'cross'),
-    (0x12f, 'square'),
-    (0x2c0, 'ps'),
-])
+codes_by_version = {
+    '14.04': {
+        'keys': dict([  # 0x01, 17
+            (0x120, 'select'),
+            (0x121, 'left_thumb'),
+            (0x122, 'right_thumb'),
+            (0x123, 'start'),
+            (0x124, 'up'),
+            (0x125, 'right'),
+            (0x126, 'down'),
+            (0x127, 'left'),
+            (0x128, 'two_left'),
+            (0x129, 'two_right'),
+            (0x12a, 'one_left'),
+            (0x12b, 'one_right'),
+            (0x12c, 'triangle'),
+            (0x12d, 'circle'),
+            (0x12e, 'cross'),
+            (0x12f, 'square'),
+            (0x2c0, 'ps'),
+        ]),
+        'misc': dict([]),  # 0x04
 
-MISC = dict([  # 0x04
-])
-
-ABS_AXES = dict([  # 0x03, value maxes at 255
-    (0x00, 'thumb_left_x'),  # left = decrease
-    (0x01, 'thumb_left_y'),  # up = decrease
-    (0x02, 'thumb_right_x'),  # left = decrease
-    (0x05, 'thumb_right_y'),  # up = decrease
-    # left?
-    (0x2c, 'up'),
-    (0x2d, 'right'),
-    (0x2e, 'down'),
-    (0x30, 'two_left'),
-    (0x31, 'two_right'),
-    (0x32, 'one_left'),
-    (0x33, 'one_right'),
-    (0x34, 'triangle'),
-    (0x35, 'circle'),
-    (0x36, 'cross'),
-    (0x37, 'square'),
-    (0x3b, 'acc_x'),  # left/right
-    (0x3c, 'acc_y'),  # forward/back
-    (0x3d, 'acc_z'),  # up/down
-])
+        'abs_axes': dict([  # 0x03, value maxes at 255
+            (0x00, 'thumb_left_x'),  # left = decrease
+            (0x01, 'thumb_left_y'),  # up = decrease
+            (0x02, 'thumb_right_x'),  # left = decrease
+            (0x05, 'thumb_right_y'),  # up = decrease
+            # left?
+            (0x2c, 'up'),
+            (0x2d, 'right'),
+            (0x2e, 'down'),
+            (0x30, 'two_left'),
+            (0x31, 'two_right'),
+            (0x32, 'one_left'),
+            (0x33, 'one_right'),
+            (0x34, 'triangle'),
+            (0x35, 'circle'),
+            (0x36, 'cross'),
+            (0x37, 'square'),
+            (0x3b, 'acc_x'),  # left/right
+            (0x3c, 'acc_y'),  # forward/back
+            (0x3d, 'acc_z'),  # up/down
+        ]),
+    },
+    '18.04': {},
+}
 
 POLL_TIMEOUT = 0.001
 THREAD_SLEEP = 0.01
@@ -90,6 +93,7 @@ def available(fn=None):
 class PS3Joystick(signaler.Signaler):
     def __init__(self, fn=None):
         super(PS3Joystick, self).__init__()
+        self.codes = None
         if fn is None:
             fn = DEFAULT_FN
         self.fn = fn
@@ -99,6 +103,18 @@ class PS3Joystick(signaler.Signaler):
         #self._settle_time = 0.4
         #self.smoothed_axes = {}
         self.report_ev_types = set((0x01, 0x03))
+
+    def lookup_name(self, code, key, default='unknown'):
+        if self.codes is None:
+            # check against possible codes
+            if key != 'keys':  # only do this for keys
+                return default
+            for v in codes_by_version:
+                if key in codes_by_version[v]['keys']:
+                    self.codes = codes_by_version[v]
+        if key not in self.codes:
+            return default
+        return self.codes[key].get(code, default)
 
     #@property
     #def settle_time(self):
@@ -125,11 +141,13 @@ class PS3Joystick(signaler.Signaler):
             'value': value}
         if ev_type == 0x01:  # keys
             e['type'] = 'button'
-            e['name'] = KEYS.get(code, 'unknown')
+            e['name'] = self.lookup_name(code, 'keys')
+            #e['name'] = KEYS.get(code, 'unknown')
             self.keys[e['name']] = value
         elif ev_type == 0x03:  # axes
             e['type'] = 'axis'
-            e['name'] = ABS_AXES.get(code, 'unknown')
+            e['name'] = self.lookup_name(code, 'abs_axes')
+            #e['name'] = ABS_AXES.get(code, 'unknown')
             self.axes[e['name']] = value
             #if e['name'] not in self.smoothed_axes:
             #    self.smoothed_axes[e['name']] = \
